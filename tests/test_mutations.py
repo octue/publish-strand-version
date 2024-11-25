80d4f912be0d6cd8bb87d653574e01958303af36
cortadocodes <cortado.codes@protonmail.com>
2024-11-25 16:22:37 +0000

    TST: Test creating a strand

--- tests/test_mutations.py
+++ tests/test_mutations.py
@@ -2,13 +2,33 @@ import unittest
 from unittest.mock import patch
 
 from publish_strand_version.exceptions import StrandsException
-from publish_strand_version.mutations import _create_strand_version
+from publish_strand_version.mutations import _create_strand, _create_strand_version
+
+
+class TestCreateStrand(unittest.TestCase):
+    def test_error_raised_if_unauthenticated(self):
+        """Test that an error is raised if trying to create a strand without authentication."""
+        with patch(
+            "gql.Client.execute",
+            return_value={"createStrand": {"messages": [{"message": "User is not authenticated."}]}},
+        ):
+            with self.assertRaises(StrandsException) as error_context:
+                _create_strand(account="some", name="strand")
+
+        self.assertEqual(error_context.exception.args[0][0]["message"], "User is not authenticated.")
+
+    def test_creating_strand(self):
+        """Test creating a strand successfully."""
+        strand_uuid = "14aca8b2-fb34-4587-a7ba-290585265d32"
+
+        with patch("gql.Client.execute", return_value={"createStrand": {"uuid": strand_uuid}}) as mock_execute:
+            response = _create_strand(account="some", name="strand")
+
+        self.assertEqual(response, strand_uuid)
+        self.assertEqual(mock_execute.mock_calls[0].kwargs["variable_values"], {"account": "some", "name": "strand"})
 
 
 class TestCreateStrandVersion(unittest.TestCase):
-    def test_error_raised_for_invalid_parameters(self):
-        pass
-
     def test_error_raised_if_unauthenticated(self):
         """Test that an error is raised if trying to create a strand version without authentication."""
         with patch(
@@ -31,7 +51,8 @@ class TestCreateStrandVersion(unittest.TestCase):
         strand_version_uuid = "14aca8b2-fb34-4587-a7ba-290585265d32"
 
         with patch(
-            "gql.Client.execute", return_value={"createStrandVersion": {"uuid": strand_version_uuid}}
+            "gql.Client.execute",
+            return_value={"createStrandVersion": {"uuid": strand_version_uuid}},
         ) as mock_execute:
             response = _create_strand_version(
                 strand="some-uuid",
