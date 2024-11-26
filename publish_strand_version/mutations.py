fbdeb695b0aa940d6664ceede3cc982249054654
cortadocodes <cortado.codes@protonmail.com>
2024-11-26 11:00:14 +0000

    ENH: Suggest semantic version for strand version if not provided

--- publish_strand_version/mutations.py
+++ publish_strand_version/mutations.py
@@ -7,7 +7,7 @@ import semver
 
 from publish_strand_version.exceptions import StrandsException
 
-STRANDS_API_URL = "https://api.octue.com/graphql/"
+STRANDS_API_URL = "http://localhost:8000/graphql/"
 STRANDS_FRONTEND_URL = "https://strands.octue.com"
 SCHEMA_REGISTRY_URL = "https://jsonschema.registry.octue.com"
 
@@ -22,9 +22,21 @@ def publish_strand_version(account, name, json_schema, version=None, notes=None)
     if not strand:
         strand = _create_strand(account, name)
 
-    strand_version_uuid = _create_strand_version(strand=strand, json_schema=json_schema, version=version, notes=notes)
-    strand_url = "/".join((STRANDS_FRONTEND_URL, account, name))
-    strand_version_uri = "/".join((SCHEMA_REGISTRY_URL, account, name, f"{version}.json"))
+    suid = f"{account}/{name}"
+    json_schema_encoded = json.dumps(json.dumps(json_schema))
+
+    if not version:
+        version = _suggest_semver(base=suid, proposed=json_schema_encoded)
+
+    strand_version_uuid = _create_strand_version(
+        strand=strand,
+        json_schema=json_schema_encoded,
+        version=version,
+        notes=notes,
+    )
+
+    strand_url = "/".join((STRANDS_FRONTEND_URL, suid))
+    strand_version_uri = "/".join((SCHEMA_REGISTRY_URL, suid, f"{version}.json"))
     return strand_url, strand_version_uri, strand_version_uuid
 
 
@@ -84,11 +96,66 @@ def _create_strand(account, name):
     return response["uuid"]
 
 
+def _suggest_semver(base, proposed):
+    parameters = {"base": base, "proposed": proposed}
+
+    query = gql.gql(
+        """
+        mutation suggestSemVer(
+            $base: String!,
+            $proposed: String!,
+        ){
+            suggestSemVer(base: $base, proposed: $proposed) {
+                ... on VersionSuggestion {
+                    suggestedVersion
+                    isBreaking
+                    isFeature
+                    isPatch
+                }
+                ... on VersionSuggestionError {
+                    type
+                    message
+                }
+                ... on OperationInfo {
+                    messages {
+                        kind
+                        message
+                        field
+                        code
+                    }
+                }
+            }
+        }
+        """
+    )
+
+    logger.info("Getting suggested semantic version...")
+    response = client.execute(query, variable_values=parameters)["suggestSemVer"]
+
+    if "messages" in response or "message" in response:
+        raise StrandsException(response.get("messages") or response.get("message"))
+
+    if response["isBreaking"]:
+        change_type = "breaking change"
+    elif response["isFeature"]:
+        change_type = "new feature"
+    else:
+        change_type = "patch change"
+
+    logger.info(
+        "The suggested semantic version is %s. This represents a %s.",
+        response["suggestedVersion"],
+        change_type,
+    )
+
+    return response["suggestedVersion"]
+
+
 def _create_strand_version(strand, json_schema, version, notes=None):
     """Create a strand version for an existing strand.
 
     :param str strand: the UUID of the strand to create a new version for
-    :param dict json_schema: the JSON schema for the strand version
+    :param str json_schema: the JSON schema for the strand version as a double-JSON-encoded string
     :param str version: the semantic version for the strand version
     :param str|None notes: any notes to associate with the strand version
     :return dict: either a successful response containing the strand version's UUID or an error response
@@ -97,7 +164,7 @@ def _create_strand_version(strand, json_schema, version, notes=None):
 
     parameters = {
         "strand": strand,
-        "json_schema": json.dumps(json.dumps(json_schema)),
+        "json_schema": json_schema,
         "major": semantic_version.major,
         "minor": semantic_version.minor,
         "patch": semantic_version.patch,
