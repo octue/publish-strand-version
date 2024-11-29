58f49ac2d046f550bf46316d7286e6ed94bf9c79
cortadocodes <cortado.codes@protonmail.com>
2024-11-29 12:23:58 +0000

    ENH: Use new token endpoint for creating a strand version

--- publish_strand_version/api.py
+++ publish_strand_version/api.py
@@ -17,9 +17,10 @@ transport = RequestsHTTPTransport(url=STRANDS_API_URL)
 client = gql.Client(transport=transport, fetch_schema_from_transport=True)
 
 
-def publish_strand_version(account, name, json_schema, version=None, notes=None):
-    """Publish a strand version for a new or existing strand.
+def publish_strand_version(token, account, name, json_schema, version=None, notes=None):
+    """Publish a strand version for an existing strand.
 
+    :param str token: a Strands access token with permission to add a new strand version to a specific strand
     :param str account: the handle of the account the strand belongs to
     :param str name: the name of the strand
     :param dict json_schema: the JSON schema to add to the strand as a strand version
@@ -27,11 +28,6 @@ def publish_strand_version(account, name, json_schema, version=None, notes=None)
     :param str notes: any notes to associate with the strand version
     :return (str, str, str):
     """
-    strand = _get_strand(account, name)
-
-    if not strand:
-        strand = _create_strand(account, name)
-
     suid = f"{account}/{name}"
     json_schema_encoded = json.dumps(json.dumps(json_schema))
 
@@ -39,7 +35,7 @@ def publish_strand_version(account, name, json_schema, version=None, notes=None)
         version = _suggest_sem_ver(base=suid, proposed=json_schema_encoded)
 
     strand_version_uuid = _create_strand_version(
-        strand=strand,
+        token=token,
         json_schema=json_schema_encoded,
         version=version,
         notes=notes,
@@ -48,80 +44,6 @@ def publish_strand_version(account, name, json_schema, version=None, notes=None)
     strand_url = "/".join((STRANDS_FRONTEND_URL, suid))
     strand_version_uri = "/".join((STRANDS_SCHEMA_REGISTRY_URL, suid, f"{version}.json"))
     return strand_url, strand_version_uri, strand_version_uuid
-
-
-def _get_strand(account, name):
-    """Query the GraphQL endpoint for a strand.
-
-    :param str account: the handle of the account the strand belongs to
-    :param str name: the name of the strand
-    :return str|None: the strand's UUID if the strand exists; `None` if it doesn't exist
-    """
-    parameters = {"account": account, "name": name}
-    suid = f"{account}/{name}"
-
-    query = gql.gql(
-        """
-        query getStrand(
-            $account: String!,
-            $name: String!,
-        ){
-            strandByName(account: $account, name: $name) {
-                ... on Strand {
-                    uuid
-                }
-                ... on DoesNotExist {
-                    message
-                }
-            }
-        }
-        """
-    )
-
-    response = client.execute(query, variable_values=parameters)["strand"]
-
-    if response:
-        if response.get("messages"):
-            raise StrandsException(response["messages"])
-
-        logger.info("Strand %r found.", suid)
-        return response["uuid"]
-
-    logger.info("Strand %r not found.", suid)
-    return None
-
-
-def _create_strand(account, name):
-    """Send a mutation to the GraphQL endpoint that creates a strand.
-
-    :param str account: the handle of an existing account to create the strand in
-    :param str name: the name to give the strand
-    :raises publish_strand_version.exceptions.StrandsException: if the mutation fails for any reason
-    :return str: the UUID of the created strand
-    """
-    parameters = {"account": account, "name": name}
-
-    query = gql.gql(
-        """
-        mutation createStrand(
-            $account: String!,
-            $name: String!,
-        ){
-            createStrand(account: $account, name: $name) {
-                uuid
-            }
-        }
-        """
-    )
-
-    logger.info("Creating strand %r...", f"{account}/{name}")
-    response = client.execute(query, variable_values=parameters)["createStrand"]
-
-    if "messages" in response:
-        raise StrandsException(response["messages"])
-
-    logger.info("Finished creating strand.")
-    return response["uuid"]
 
 
 def _suggest_sem_ver(base, proposed):
@@ -186,10 +108,10 @@ def _suggest_sem_ver(base, proposed):
     return response["suggestedVersion"]
 
 
-def _create_strand_version(strand, json_schema, version, notes=None):
+def _create_strand_version(token, json_schema, version, notes=None):
     """Send a mutation to the GraphQL endpoint that creates a strand version for an existing strand.
 
-    :param str strand: the UUID of the strand to create a new version for
+    :param str token: a Strands access token with permission to add a new strand version to a specific strand
     :param str json_schema: the JSON schema for the strand version as a double-JSON-encoded string
     :param str version: the semantic version for the strand version
     :param str|None notes: any notes to associate with the strand version
@@ -198,7 +120,7 @@ def _create_strand_version(strand, json_schema, version, notes=None):
     semantic_version = semver.Version.parse(version)
 
     parameters = {
-        "strand": strand,
+        "strand_token": token,
         "json_schema": json_schema,
         "major": semantic_version.major,
         "minor": semantic_version.minor,
@@ -209,8 +131,8 @@ def _create_strand_version(strand, json_schema, version, notes=None):
 
     query = gql.gql(
         """
-        mutation createStrandVersion(
-            $strand: String!,
+        mutation createStrandVersionViaToken(
+            $strand_token: String!,
             $json_schema: String!,
             $major: String!,
             $minor: String!,
@@ -219,7 +141,7 @@ def _create_strand_version(strand, json_schema, version, notes=None):
             $notes: String
         ) {
             createStrandVersion(
-                strandUuid: $strand,
+                strandToken: $strand_token,
                 jsonSchema: $json_schema,
                 major: $major,
                 minor: $minor,
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,31 +2,18 @@ import json
 import unittest
 from unittest.mock import patch
 
-from publish_strand_version.api import (
-    _create_strand,
-    _create_strand_version,
-    _get_strand,
-    _suggest_sem_ver,
-    publish_strand_version,
-)
+from publish_strand_version.api import _create_strand_version, _suggest_sem_ver, publish_strand_version
 from publish_strand_version.exceptions import StrandsException
 
 
 class TestPublishStrandVersion(unittest.TestCase):
-    def test_with_non_existent_strand(self):
-        """Test publishing a strand version for a non-existent strand."""
-        strand_uuid = "14aca8b2-fb34-4587-a7ba-290585265d32"
+    def test_with_existing_strand(self):
+        """Test publishing a strand version for an existing strand."""
         expected_strand_version_uuid = "e75dd480-4bfa-4ae9-b5c0-853e9a114194"
 
-        with patch(
-            "gql.Client.execute",
-            side_effect=[
-                {"strand": None},
-                {"createStrand": {"uuid": strand_uuid}},
-                {"createStrandVersion": {"uuid": expected_strand_version_uuid}},
-            ],
-        ):
+        with patch("gql.Client.execute", side_effect=[{"createStrandVersion": {"uuid": expected_strand_version_uuid}}]):
             strand_url, strand_version_uri, strand_version_uuid = publish_strand_version(
+                token="some-token",
                 account="some",
                 name="strand",
                 json_schema={"some": "schema"},
@@ -36,72 +23,6 @@ class TestPublishStrandVersion(unittest.TestCase):
         self.assertEqual(strand_url, "https://strands.octue.com/some/strand")
         self.assertEqual(strand_version_uri, "https://jsonschema.registry.octue.com/some/strand/1.0.0.json")
         self.assertEqual(strand_version_uuid, expected_strand_version_uuid)
-
-    def test_with_existing_strand(self):
-        """Test publishing a strand version for an existing strand."""
-        strand_uuid = "14aca8b2-fb34-4587-a7ba-290585265d32"
-        expected_strand_version_uuid = "e75dd480-4bfa-4ae9-b5c0-853e9a114194"
-
-        with patch(
-            "gql.Client.execute",
-            side_effect=[
-                {"strand": {"uuid": strand_uuid}},
-                {"createStrandVersion": {"uuid": expected_strand_version_uuid}},
-            ],
-        ):
-            with patch("publish_strand_version.api._create_strand") as mock_create_strand:
-                strand_url, strand_version_uri, strand_version_uuid = publish_strand_version(
-                    account="some",
-                    name="strand",
-                    json_schema={"some": "schema"},
-                    version="1.0.0",
-                )
-
-        mock_create_strand.assert_not_called()
-        self.assertEqual(strand_url, "https://strands.octue.com/some/strand")
-        self.assertEqual(strand_version_uri, "https://jsonschema.registry.octue.com/some/strand/1.0.0.json")
-        self.assertEqual(strand_version_uuid, expected_strand_version_uuid)
-
-
-class TestGetStrand(unittest.TestCase):
-    def test_none_returned_for_non_existent_strand(self):
-        """Test that `None` is returned if trying to get a non-existent strand."""
-        with patch("gql.Client.execute", return_value={"strand": None}):
-            response = _get_strand(account="some", name="strand")
-
-        self.assertIsNone(response)
-
-    def test_strand_uuid_returned_for_existing_strand(self):
-        """Test that the strand's UUID is returned if the strand exists."""
-        strand_uuid = "14aca8b2-fb34-4587-a7ba-290585265d32"
-
-        with patch("gql.Client.execute", return_value={"strand": {"uuid": strand_uuid}}):
-            response = _get_strand(account="some", name="strand")
-
-        self.assertEqual(response, strand_uuid)
-
-
-class TestCreateStrand(unittest.TestCase):
-    def test_error_raised_if_unauthenticated(self):
-        """Test that an error is raised if trying to create a strand without authentication."""
-        with patch(
-            "gql.Client.execute",
-            return_value={"createStrand": {"messages": [{"message": "User is not authenticated."}]}},
-        ):
-            with self.assertRaises(StrandsException) as error_context:
-                _create_strand(account="some", name="strand")
-
-        self.assertEqual(error_context.exception.args[0][0]["message"], "User is not authenticated.")
-
-    def test_creating_strand(self):
-        """Test creating a strand successfully."""
-        strand_uuid = "14aca8b2-fb34-4587-a7ba-290585265d32"
-
-        with patch("gql.Client.execute", return_value={"createStrand": {"uuid": strand_uuid}}) as mock_execute:
-            response = _create_strand(account="some", name="strand")
-
-        self.assertEqual(response, strand_uuid)
-        self.assertEqual(mock_execute.mock_calls[0].kwargs["variable_values"], {"account": "some", "name": "strand"})
 
 
 class TestSuggestSemVer(unittest.TestCase):
@@ -142,7 +63,7 @@ class TestCreateStrandVersion(unittest.TestCase):
             return_value={"createStrandVersion": {"messages": [{"message": "User is not authenticated."}]}},
         ):
             with self.assertRaises(StrandsException) as error_context:
-                _create_strand_version(strand="some-uuid", json_schema='"{}"', version="0.1.0")
+                _create_strand_version(token="some-token", json_schema='"{}"', version="0.1.0")
 
         self.assertEqual(error_context.exception.args[0][0]["message"], "User is not authenticated.")
 
@@ -155,7 +76,7 @@ class TestCreateStrandVersion(unittest.TestCase):
             return_value={"createStrandVersion": {"uuid": strand_version_uuid}},
         ) as mock_execute:
             response = _create_strand_version(
-                strand="some-uuid",
+                token="some-token",
                 json_schema='"{\\"some\\": \\"schema\\"}"',
                 version="0.1.0-rc.1",
             )
@@ -165,7 +86,7 @@ class TestCreateStrandVersion(unittest.TestCase):
         self.assertEqual(
             mock_execute.mock_calls[0].kwargs["variable_values"],
             {
-                "strand": "some-uuid",
+                "strand_token": "some-token",
                 "json_schema": '"{\\"some\\": \\"schema\\"}"',
                 "major": 0,
                 "minor": 1,
